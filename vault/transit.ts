dbf279d59803744312c3e2e201bd7c6d060bf2e1
Rupesh Kumar <rupesh@amt.in>
2025-03-26 19:44:29 +0530

    Added env for vault connection.

--- vault/transit.ts
+++ vault/transit.ts
@@ -1,35 +1,50 @@
-import * as fs from 'fs';
-import axios from 'axios'
+import * as fs from "fs";
+import axios from "axios";
 
-axios.get('http://localhost:8200/v1/sys/init').then(async (res) => {
-    if(!res.data.initialized) throw new Error('vault is not initialized')
+console.log("transient vault...", process.env.VAULT_URL);
+
+axios
+  .get(`${process.env.VAULT_URL || "http://localhost:8200"}/v1/sys/init`)
+  .then(async (res) => {
+    if (!res.data.initialized) throw new Error("vault is not initialized");
 
     // fetch root token
-    const rootToken: string = JSON.parse(fs.readFileSync('vault-seal-keys.json').toString()).root_token
+    const rootToken: string = JSON.parse(
+      fs.readFileSync("vault-seal-keys.json").toString()
+    ).root_token;
 
     // list mounts
-    const mounts = await axios.get('http://localhost:8200/v1/sys/mounts', {
+    const mounts = await axios.get(
+      `${process.env.VAULT_URL || "http://localhost:8200"}/v1/sys/mounts`,
+      {
         headers: {
-            'X-Vault-Token': rootToken
-        }
-    })
-    console.log(`mounts : ${JSON.stringify(mounts.data)}`)
+          "X-Vault-Token": rootToken,
+        },
+      }
+    );
+    console.log(`mounts : ${JSON.stringify(mounts.data)}`);
 
     // mount transit engine through POST
-    const mountTransitEngine = await axios.post('http://localhost:8200/v1/sys/mounts/transit', {
-        type: 'transit',
+    const mountTransitEngine = await axios.post(
+      `${process.env.VAULT_URL || "http://localhost:8200"}/v1/sys/mounts/transit`,
+      {
+        type: "transit",
         config: {
-            force_no_cache: true
-        }
-    }, {
+          force_no_cache: true,
+        },
+      },
+      {
         headers: {
-            'X-Vault-Token': rootToken
-        }
-    })
+          "X-Vault-Token": rootToken,
+        },
+      }
+    );
 
     // log mount response
-    console.log(`mount transit engine : ${JSON.stringify(mountTransitEngine.data)}`)
-}
-).catch((err) => {
-    console.log(`err : ${JSON.stringify(err)}`)
-})
+    console.log(
+      `mount transit engine : ${JSON.stringify(mountTransitEngine.data)}`
+    );
+  })
+  .catch((err) => {
+    console.log(`err : ${JSON.stringify(err)}`);
+  });
